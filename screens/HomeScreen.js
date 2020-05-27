12638c4c6b336e060e659d4e45f3aac65e902920
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-05-27 15:03:35 -0300

    homeScreen with sendRight

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, View, Button, Platform, StatusBar } from 'react-native';
 import { ScrollView,  } from 'react-native-gesture-handler';
-import Send from '../components/SendForm';
+import Send from '../components/SendRight';
 
 export default function HomeScreen() {
   return (
